a30e1b5c188e5889383503549b725ea19206b884
DESKTOP-IL4GEF4\EZEN <yangjeonghun997@gmail.com>
2026-04-21 17:07:23 +0900

    임시 홈페이지 구성

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import MyLibrary from "./MyLibrary";
+import Recommend from "./Recommend";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home-container">
+      <header className="home-header">
+        <h1>📚 독서 대시보드</h1>
+        <p>오늘도 한 페이지 성장하세요</p>
+      </header>
+
+      <main className="home-main">
+        {/* 추천 */}
+        <section className="section">
+          추천도서 섹션
+          {/* <Recommend /> */}
+        </section>
+        {/* 서재 + 통계 */}
+        <section className="section">
+          <Link to="/mypage">마이페이지</Link>
+          {/* <MyLibrary /> */}
+        </section>
+        {/* 로그인 */}
+        <Link to="/login">로그인</Link>
+      </main>
+    </div>
+  );
 };
 
 export default Home;
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 
 const MyPage = () => {
   return (
     <>
       <div>MyPage</div>
+      <ul>
+        <li>
+          최근에 읽은 책<Link to="/book">책 상세 페이지</Link>
+        </li>
+        <li>읽을 예정인 책</li>
+        <li>읽는 중인 책</li>
+      </ul>
+      <NavLink to="/mypage/statistics">내 독서 통계</NavLink>
       <Outlet />
     </>
   );
