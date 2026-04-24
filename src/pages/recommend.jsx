c0e03ec703374910365854c6460ebdc4edfda705
DESKTOP-IL4GEF4\EZEN <yangjeonghun997@gmail.com>
2026-04-24 16:07:40 +0900

    .

--- src/pages/recommend.jsx
+++ src/pages/recommend.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // 필요한 컴포넌트나 라이브러리를 여기서 임포트하세요.
 // 예: import { Link } from "react-router-dom";
 import "./recommend.scss"; // 혹은 CSS 파일
+import useStore from "./Store/store";
 
 const Recommend = () => {
   // 만약 추천 도서 데이터가 있다면 여기서 관리하거나 props로 받으세요.
@@ -151,11 +152,11 @@ const Recommend = () => {
       <div className="recommend-header">
         <h2 className="section-title">오늘의 추천 도서</h2>
       </div>
-      
+
       <div className="recommend-list">
         {/* 추천 도서 아이템들을 렌더링하는 로직 */}
         <p>AI가 분석한 맞춤형 도서 리스트입니다.</p>
-        
+
         {/* 예시 리스트 렌더링 */}
         <div className="recommend-grid">
           {/* recommendedBooks.map(book => (...)) */}
