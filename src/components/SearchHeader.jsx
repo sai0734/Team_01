6e9b3db14da3ec1e7ce39b41da7b7cd185e110eb
DESKTOP-IL4GEF4\EZEN <yangjeonghun997@gmail.com>
2026-04-24 11:59:46 +0900

    주석, className 추가, searchpage css 분리

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -11,21 +11,18 @@ const SearchHeader = () => {
   };
 
   return (
-    <div>
-      {/* 상단 검색 영역 */}
-      <div>
-        <input
-          type="text"
-          name="bookSearch"
-          placeholder="책 제목, 저자, 검색..."
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          onKeyPress={(e) => e.key === "Enter" && onClick()}
-        />
-        <button type="button" onClick={onClick}>
-          검색
-        </button>
-      </div>
+    <div className="searchHeader">
+      <input
+        type="text"
+        name="bookSearch"
+        placeholder="책 제목, 저자, 검색..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyPress={(e) => e.key === "Enter" && onClick()}
+      />
+      <button className="searchButton" type="button" onClick={onClick}>
+        검색
+      </button>
     </div>
   );
 };
