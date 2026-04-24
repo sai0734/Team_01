d7fcc8e8df62a4ca2814c27dbf0a123cec1836eb
DESKTOP-IL4GEF4\EZEN <yangjeonghun997@gmail.com>
2026-04-24 12:20:16 +0900

    홈화면 서재 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Recommend from "./recommend"; 
+import Recommend from "./recommend";
 import Mylibrary from "./mylibrary";
-import "./Home.scss"; 
+import "./Home.scss";
+import useStore from "./Store/store";
 
 const Home = () => {
+  const { booksList } = useStore();
+
+  // 읽고 있는 책 서재
+  const readingBooks = booksList.filter(
+    (book) => book.status == "읽고 있는 책",
+  );
+  const renderBookGrid = (books) => {
+    if (books.length === 0) {
+      return <p className="empty-msg">서재에 담긴 책이 없습니다.</p>;
+    }
+
+    return (
+      <div className="book-grid">
+        {books.map((book) => (
+          <div key={book.isbn} className="book-item">
+            <Link to={`/book?isbn=${book.isbn}`}>
+              <img
+                src={
+                  book.thumbnail ||
+                  "https://via.placeholder.com/120x174?text=No+Image"
+                }
+                alt={book.title}
+              />
+            </Link>
+            <div className="book-info">
+              <p className="book-title">{book.title}</p>
+              {book.rating > 0 && (
+                <p className="book-rating">★ {book.rating}</p>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
   return (
     <div className="home-container">
       <header className="home-header">
@@ -17,7 +54,7 @@ const Home = () => {
         <nav className="home-nav">
           <Link to="/mypage">나의 서재 가기</Link>
           {/* 폴더 구조상 상세페이지 경로는 /BookDetailPage 등이 될 수 있습니다. */}
-          <Link to="/book-detail">도서 상세페이지</Link> 
+          <Link to="/book-detail">도서 상세페이지</Link>
         </nav>
 
         {/* 2. 고민 기반 AI 도서 추천 섹션 */}
@@ -26,14 +63,15 @@ const Home = () => {
           <Recommend />
         </section>
 
-        {/* 3. 읽고 있는 책들 (서재) 섹션
-        <section className="section">
-          <div className="section-header">
-            <h2 className="section-title">2. 현재 읽고 있는 책</h2>
-            <Link to="/mypage" className="more-link">내 서재 가기 →</Link>
+        {/* 3. 읽고 있는 책들 (서재) 섹션 */}
+        <section className={`shelf-section`}>
+          <div className="shelf-header">
+            <h3>
+              읽고 있는 책 <span>({readingBooks.length})</span>
+            </h3>
           </div>
-          <Mylibrary />
-        </section> */}
+          <div className="shelf-content">{renderBookGrid(readingBooks)}</div>
+        </section>
       </main>
     </div>
   );
